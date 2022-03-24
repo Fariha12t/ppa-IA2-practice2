61395b86a4efc26f1e26cd2958dd07e0745b2b99
Fariha12t <96039101+Fariha12t@users.noreply.github.com>
2022-03-24 10:04:39 +0000

    str reverse

--- p5original.c
+++ p5original.c
@@ -1,4 +1,36 @@
-#include <stdio.h>
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter any number:\n");
+  scanf("%d",&a);
+  return a;
+}
+int gcd(int a,int b)
+{
+  int hcf;
+  for(int i=2;i<a && i<b;i++)
+  {
+    if(a%i==0 && b%i==0)
+    {
+      hcf=i;
+    }
+  }
+  return hcf;
+}
+void output(int a,int b,int hcf)
+{
+  printf("hcf of %d and %d is %d",a,b,hcf);
+}
+int main()
+{
+  int a,b,hcf;
+  a=input();
+  b=input();
+  hcf=gcd(a,b);
+  output(a,b,hcf);
+}
+/*#include <stdio.h>
 
 int input()
 {
@@ -43,4 +75,4 @@ int main()
   int res = gcd(n1, n2);
   output(n1, n2, res);
   return 0;
-}
+}*/
--- p6original.c
+++ p6original.c
@@ -5,46 +5,35 @@ void input_string(char *a)
   printf("Enter the string:\n");
   scanf("%s", a);
 }
-int string_length(char *str)
+
+int str_reverse(char *a)
 {
-  int n;
-  for (n = 0; str[n]; n++);
-  return n;
+  int b;
+  for(int i=0;a[i]!='\0';i++)
+    {
+      b=i;
+    }
+ 
+  return b;
 }
 
-void string_copy(char *d, char *s)
-{
-  for(int i=0;s[i]!='\0';i++)
-    d[i]=s[i];
-}
+void output(char *a, int reverse_a)
 
-char *str_reverse(char *str)
 {
-  int n,t;
-  n = string_length(str);
-  n--;
-  for (int i = 0; i< n/2; i++)
-  {
-     t= str[i];
-     str[i] = str[n-i];
-     str[n-i] = t;
-  }
-  return str;
-}
-
-void output(char *a, char *reverse_a)
-{
-  printf("The reverse of %s is %s\n", a, reverse_a);
+  printf("reverse string is ");
+  for(int b=reverse_a;b>=0;b--)
+    printf("%c",a[b]);  
+  
 }
 
 int main()
 {
-  char str[100];
-  input_string(str);
-  char rev_str[100];
-  string_copy(rev_str,str);
-  str_reverse(rev_str);
-  output(str, rev_str);
+  char a[100];
+  input_string(a);
+  int b;
+ 
+  b=str_reverse(a);
+  output(a,b);
   return 0;
 }
 
